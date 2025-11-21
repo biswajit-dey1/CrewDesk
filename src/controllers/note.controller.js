209667eecc95e803274cbf63078ba26eabc4c228
biswajit-dey1 <deyb18860@gmail.com>
2025-11-21 14:07:52 +0530

    feat: Enhance notes functionality with CRUD operations and routing updates

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,57 +1,195 @@
-import mongoose from "mongoose"
-import ProjectNote from "../models/Note.model.js"
-import Project from "../models/Project.model.js"
+import mongoose from "mongoose";
+import ProjectNote from "../models/Note.model.js";
+import Project from "../models/Project.model.js";
 
 const createNote = async (req, res) => {
+    const { content } = req.body;
 
-    const { content } = req.body
-
-    const { projectId } = req.params
+    const { projectId } = req.params;
 
     try {
+        const project = await Project.findById(projectId);
 
-        const project = await Project.findById(projectId)
-        console.log(project)
 
         if (!project) {
-            return res.status(404)
-                .json({
-                    succes: false,
-                    message: "Project not found "
-                })
+            return res.status(404).json({
+                succes: false,
+                message: "Project not found ",
+            });
         }
 
         const projectNote = await ProjectNote.create({
             project: new mongoose.Types.ObjectId(projectId),
             createdBy: new mongoose.Types.ObjectId(req.user._id),
-            content
-        })
-  
-        console.log(projectNote)
-        const populateProjectNote = await ProjectNote.findById(projectNote._id)
-            .populate("createdB690ef7548a261a29b2660fe1y",
-                "userName fullName avatar"
-            )
+            content,
+        });
 
-     console.log(populateProjectNote)
+        const populateProjectNote = await ProjectNote.findById(
+            projectNote._id
+        ).populate("createdBy", "userName fullName avatar");
+
+        return res.status(201).json({
+            succes: true,
+            message: "ProjectNote created Succesfully",
+            populateProjectNote,
+        });
+    } catch (error) {
+        return res.status(501).json({
+            succes: false,
+            message: error.message,
+        });
+    }
+};
+
+const getNotes = async (req, res) => {
+    const { projectId } = req.params;
+
+    try {
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            return res.status(404).json({
+                succes: false,
+                message: "Project not found",
+            });
+        }
+
+        const projectNote = await ProjectNote.find({
+            project: new mongoose.Types.ObjectId(projectId),
+        }).populate("createdBy", "userName fullName avatar");
+
+        if (!projectNote) {
+            return res.status(404).json({
+                succes: false,
+                message: "ProjectNote not found",
+            });
+        }
+
+        return res.status(200).json({
+            succes: true,
+            message: "ProjectNote Fetched Succesfully",
+            projectNote,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(501).json({
+            succes: false,
+            message: error.message,
+        });
+    }
+};
+
+const getNoteById = async (req, res) => {
+    const { noteId } = req.params;
+
+
+    try {
+        const projectNote = await ProjectNote.findById(noteId).populate(
+            "createdBy",
+            "userName fullName avatar"
+        );
+
+
+        if (!projectNote) {
+            return res.status(404).json({
+                succes: false,
+                message: "ProjectNote not found",
+            });
+        }
+
+        return res.status(200).json({
+            succes: true,
+            message: "ProjectNote Fetched Succesfully",
+            projectNote,
+        });
+
+    } catch (error) {
+        return res.status(501).json({
+            succes: false,
+            message: error.message,
+        });
+    }
+};
+
+const updateProjectNote = async (req, res) => {
+    const { noteId } = req.params
+    const { content } = req.body
+
+    try {
+        const existingNote = await ProjectNote.findById(noteId)
+
+        if (!existingNote) {
+            return res.status(404)
+                .json({
+                    succes: false,
+                    message: "Note not found"
+
+                })
+        }
+
+        const updatedNote = await ProjectNote.findByIdAndUpdate(
+            noteId,
+            {
+                content
+            },
+            { new: true }
+        ).populate(
+            "createdBy",
+            "userName fullName avatar"
+        )
 
         return res.status(201)
             .json({
                 succes: true,
-                message: "ProjectNote created Succesfully",
-                populateProjectNote
-            })
+                message: "Note updated succesfully",
+                updatedNote
 
-    } catch (error) {
-        return res.status(501)
-            .json({
-                succes: false,
-                message: error.message
             })
+    } catch (error) {
+        return res.status(501).json({
+            succes: false,
+            message: error.message,
+        });
     }
 
 }
 
-export {
-    createNote
+const deleteNote = async (req, res) => {
+    const { noteId } = req.params
+    try {
+        const note = await ProjectNote.findByIdAndDelete(noteId)
+
+        if (!note) {
+            return res.status(404)
+                .json({
+                    succes: false,
+                    message: "Note not found"
+                })
+
+        }
+
+
+        return res.status(200)
+            .json({
+                succes: true,
+                message: "Note deleted Succesfully",
+                note
+            })
+
+
+
+    } catch (error) {
+        return res.status(501).json({
+            succes: false,
+            message: error.message,
+        });
+    }
 }
+export {
+    createNote,
+    getNotes,
+    getNoteById,
+    updateProjectNote,
+    deleteNote
+};
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,12 +1,17 @@
 import express from "express"
 import isLoggedIn, { validateProjectPermission } from "../middlewares/auth.middleware.js"
-import { UserRoleEnum } from "../utils/constant.js"
-import { createNote } from "../controllers/note.controller.js"
+import { AvailableUserRole, UserRoleEnum } from "../utils/constant.js"
+import { createNote, deleteNote, getNoteById, getNotes, updateProjectNote } from "../controllers/note.controller.js"
 
 
 
 
 const notesRouter = express.Router()
 notesRouter.post("/:projectId/createNote",isLoggedIn,validateProjectPermission([UserRoleEnum.ADMIN]),createNote)
+notesRouter.get("/:projectId/getNotes",isLoggedIn,validateProjectPermission(AvailableUserRole),getNotes)
+notesRouter.get("/:projectId/getNoteById/:noteId",isLoggedIn,validateProjectPermission(AvailableUserRole),getNoteById)
+notesRouter.patch("/:projectId/updateNote/:noteId",isLoggedIn,validateProjectPermission([UserRoleEnum.ADMIN]),updateProjectNote)
+
+notesRouter.delete("/:projectId/deleteNote/:noteId",isLoggedIn,validateProjectPermission([UserRoleEnum.ADMIN]),deleteNote)
 
 export default notesRouter
